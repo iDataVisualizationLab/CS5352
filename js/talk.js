363acd1e3b699a1c1efc7a545c7c276f3bb87cb4
Zipexpo <nvtngan@hcmus.edu.vn>
2020-03-16 17:07:29 -0500

    update project1 selection

--- js/talk.js
+++ js/talk.js
@@ -140,19 +140,5 @@ d3.json('data/students.json').then(function (data) {
         }
         return '#13d9b7'
     }
-})})
+})});
 
-function daysBetween(first, second) {
-
-    // Copy date parts of the timestamps, discarding the time parts.
-    var one = new Date(first.getFullYear(), first.getMonth(), first.getDate());
-    var two = new Date(second.getFullYear(), second.getMonth(), second.getDate());
-
-    // Do the math.
-    var millisecondsPerDay = 1000 * 60 * 60 * 24;
-    var millisBetween = two.getTime() - one.getTime();
-    var days = millisBetween / millisecondsPerDay;
-
-    // Round down.
-    return Math.floor(days);
-}
